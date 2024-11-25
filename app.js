4b4c569acea1aeccf00135d706b3b3d1b18b7913
Nat <64110681+vidmontng@users.noreply.github.com>
2024-11-25 18:55:29 -0500

    Redirects

--- app.js
+++ app.js
@@ -31,11 +31,12 @@ app.get('/cards', (req, res) => {
 
 app.get('/hello', (req, res) => {
     res.render('hello', {name: req.cookies.username});
+
 });
 
 app.post('/hello', (req, res) => {
     res.cookie('username', req.body.username);
-    res.render('hello', {name: req.body.username});
+    res.redirect('/');
 });
 
 
